cf0d4b8af0989d49fb7004b1695f5a7515be4d31
David Zong <davidzng@uw.edu>
2015-03-05 14:54:48 -0600

    Wrote a few lines of importing and such

--- ledfunction.py
+++ ledfunction.py
@@ -1,2 +1,12 @@
 # This script allows the user to choose between a list of periodic functions and
 # make the LED shine light in that manner
+
+## Import packages
+import time
+import RPi.GPIO as GPIO
+import numpy as np
+
+## Define GPIO pins and set the GPIO out pin to 11
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+
